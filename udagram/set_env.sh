6b49076ca217883e10b0d173de11f3b8bef54564
Mohammed Fared Rezk <mrezk.c@saip.gov.sa>
2023-03-14 21:27:31 +0300

    initl commit

---
+++ udagram/set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+# export POSTGRES_USERNAME=postgres
+# export POSTGRES_PASSWORD=postgres1234
+# export POSTGRES_HOST=database-1.cplz0g5qu4dc.us-east-1.rds.amazonaws.com
+# export POSTGRES_DB=postgres
+# export AWS_BUCKET=arn:aws:s3:::storebucket-1132023
+# export AWS_REGION=us-east-2
+# export AWS_PROFILE=default
+# export JWT_SECRET=mysecretstring
+# export URL=http://localhost:3000
