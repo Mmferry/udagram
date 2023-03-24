5f254db6f964a5c814b1f67c2724a3cb0bf55589
Mohammed Fared Rezk <mrezk.c@saip.gov.sa>
2023-03-24 22:09:20 +0300

    .npmrc in project root added .elasticbeanstalk in copy

--- udagram/set_env.sh
+++ udagram/set_env.sh
@@ -2,10 +2,10 @@
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 # export POSTGRES_USERNAME=postgres
 # export POSTGRES_PASSWORD=postgres1234
-# export POSTGRES_HOST=database-1.cplz0g5qu4dc.us-east-1.rds.amazonaws.com
-# export POSTGRES_DB=postgres
-# export AWS_BUCKET=arn:aws:s3:::storebucket-1132023
-# export AWS_REGION=us-east-2
+# export POSTGRES_HOST=udagram.cplz0g5qu4dc.us-east-1.rds.amazonaws.com
+# export POSTGRES_DB=udagram
+# export AWS_BUCKET=arn:aws:s3:::udagram-bucket1332023
+# export AWS_REGION=us-east-1
 # export AWS_PROFILE=default
 # export JWT_SECRET=mysecretstring
-# export URL=http://localhost:3000
+# export URL=http://udagram-bucket1332023.s3-website-us-east-1.amazonaws.com
